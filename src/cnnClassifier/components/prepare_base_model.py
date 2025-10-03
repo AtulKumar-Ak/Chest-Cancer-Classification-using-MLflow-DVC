ddd933c7c97588f961422c4c73bca2484a63a926
AtulKumar <akatulkumar0001@gmail.com>
2025-10-04 03:48:14 +0530

    model trainer added and optimizer changed to adam

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -9,7 +9,6 @@ class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
 
-    
     def get_base_model(self):
         self.model = tf.keras.applications.vgg16.VGG16(
             input_shape=self.config.params_image_size,
@@ -19,50 +18,77 @@ class PrepareBaseModel:
 
         self.save_model(path=self.config.base_model_path, model=self.model)
 
-
-    
     @staticmethod
     def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
+        # CRITICAL FIX: Unfreeze last few layers for better learning
         if freeze_all:
-            for layer in model.layers:
-                model.trainable = False
+            # Freeze all base model layers
+            model.trainable = False
         elif (freeze_till is not None) and (freeze_till > 0):
+            # Freeze all layers first
+            model.trainable = True
+            # Then freeze only the first layers, keeping last layers trainable
             for layer in model.layers[:-freeze_till]:
-                model.trainable = False
+                layer.trainable = False
+        else:
+            # Train all layers
+            model.trainable = True
 
+        # Add custom classification head
         flatten_in = tf.keras.layers.Flatten()(model.output)
+        
+        # Add dropout for regularization
+        dropout1 = tf.keras.layers.Dropout(0.5)(flatten_in)
+        
+        # Add a dense layer before final classification
+        dense1 = tf.keras.layers.Dense(
+            units=256,
+            activation="relu"
+        )(dropout1)
+        
+        dropout2 = tf.keras.layers.Dropout(0.3)(dense1)
+        
+        # Final classification layer
         prediction = tf.keras.layers.Dense(
             units=classes,
             activation="softmax"
-        )(flatten_in)
+        )(dropout2)
 
         full_model = tf.keras.models.Model(
             inputs=model.input,
             outputs=prediction
         )
 
+        # Use Adam optimizer instead of SGD
         full_model.compile(
-            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
             loss=tf.keras.losses.CategoricalCrossentropy(),
             metrics=["accuracy"]
         )
 
         full_model.summary()
+        
+        # Print trainable vs non-trainable parameters
+        trainable_params = sum([tf.size(w).numpy() for w in full_model.trainable_weights])
+        non_trainable_params = sum([tf.size(w).numpy() for w in full_model.non_trainable_weights])
+        print(f"\n{'='*60}")
+        print(f"Trainable parameters: {trainable_params:,}")
+        print(f"Non-trainable parameters: {non_trainable_params:,}")
+        print(f"{'='*60}\n")
+        
         return full_model
     
-
     def update_base_model(self):
+        # CHANGED: Unfreeze last 4 layers instead of freezing all
         self.full_model = self._prepare_full_model(
             model=self.model,
             classes=self.config.params_classes,
-            freeze_all=True,
-            freeze_till=None,
+            freeze_all=False,  # Changed from True
+            freeze_till=4,     # Unfreeze last 4 layers
             learning_rate=self.config.params_learning_rate
         )
 
         self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
-    
-
 
     @staticmethod
     def save_model(path: Path, model: tf.keras.Model):
--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from src.cnnClassifier.constants import *
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig)
 class ConfigurationManager:
     def __init__(
         self,
@@ -31,7 +31,6 @@ class ConfigurationManager:
 
         create_directories([self.config.artifacts_root])
 
-
     def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
         
@@ -49,3 +48,44 @@ class ConfigurationManager:
         )
 
         return prepare_base_model_config
+    
+
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        
+        # Updated paths for pre-split data
+        base_dir = os.path.join(self.config.data_ingestion.unzip_dir, "Data")
+        training_data = os.path.join(base_dir, "train")
+        validation_data = os.path.join(base_dir, "valid")
+        test_data = os.path.join(base_dir, "test")
+        
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            validation_data=Path(validation_data),
+            test_data=Path(test_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
